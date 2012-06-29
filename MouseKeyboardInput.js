ac1a3f641e5d100390cd22e1382a9738f336e7ef
Andrew <andrewboudreau@gmail.com>
2012-06-29 01:42:10 -0500

    fixed tabbing
    whitespace issues

--- MouseKeyboardInput.js
+++ MouseKeyboardInput.js
@@ -50,15 +50,15 @@ MouseKeyboardInput = {
 	},
 	
 	findPos: function(obj) {
-    var curleft = 0, curtop = 0;
-    if (obj.offsetParent) {
-        do {
-            curleft += obj.offsetLeft;
-            curtop += obj.offsetTop;
-        } while (obj = obj.offsetParent);
-        return [ curleft, curtop ];
-    }
-    return undefined;
+		var curleft = 0, curtop = 0;
+		if (obj.offsetParent) {
+			do {
+				curleft += obj.offsetLeft;
+				curtop += obj.offsetTop;
+			} while (obj = obj.offsetParent);
+			return [ curleft, curtop ];
+		}
+		return undefined;
 	},
 
 	isPressed: function(keyCode) {
