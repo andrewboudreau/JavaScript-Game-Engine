33373cd58a412d13344f916db55b620860cedf13
Andrew <andrewboudreau@gmail.com>
2012-07-09 21:37:33 -0500

    changing singleton pattern

--- EntityManager.js
+++ EntityManager.js
@@ -1,58 +1,66 @@
-(function(scope) {
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, curly:true, browser:true, indent:4, maxerr:50, newcap:true, white:false */
+
+/*global Game Entity */
+(function (scope) {
+	"use strict";
+	
 	if (!Entity) {
 		throw "Class.Entity is not defined and is required.";
 	}
 	
-	function _EntityManager() {
+	function EntityManager() {
+		if ( EntityManager.prototype._singletonInstance ) {
+			return EntityManager.prototype._singletonInstance;
+		}
+		
+		EntityManager.prototype._singletonInstance = this;
 		this.entities = [];
-	}
-	
-	_EntityManager.prototype = {
-		guard: function(entity) {
+		
+		this.guard = function(entity) {
 			if ( !(entity instanceof Entity) ) {
 				throw "entity must be an instance of Entity";
 			}	
-		},
+		};
 		
-		add: function( entity ) {	
-			EntityManager.guard(entity);
+		this.add = function( entity ) {	
+			this.guard(entity);
 			
-			EntityManager.entities.push(entity);
+			this.entities.push(entity);
 			return entity;
-		},
+		};
 		
-		remove: function( entity ) {
-			var index = EntityManager.entities.indexOf(entity);
+		this.remove = function( entity ) {
+			var index = this.entities.indexOf(entity);
 			if( index !== -1) {
-				EntityManager.entities.splice(index, 1);
+				this.entities.splice(index, 1);
 			}
-		},
+		};
 		
-		clear: function() {
-			EntityManager.entities.length = 0;
-		},
+		this.clear = function() {
+			this.entities.length = 0;
+		};
 		
-		indexOf: function( entity ) {
-			EntityManager.guard(entity);
+		this.indexOf = function( entity ) {
+			this.guard(entity);
 			
-			return EntityManager.entities.indexOf( entity );
-		},
+			return this.entities.indexOf( entity );
+		};
 		
-		item: function(index) {
+		this.item = function(index) {
 			if(index < 0 || index >= this.entities.length) {
 				throw "argument out of range";
 			}
 			
-			return EntityManager.entities[index];
-		},
+			return this.entities[index];
+		};
 	
-		each: function(action) {
-			for(var i = 0; i < EntityManager.entities.length; i++) {
-				action.apply(Game, EntityManager.entities[i]);
+		this.each = function(action) {
+			for(var i = 0; i < this.entities.length; i++) {
+				action.apply(Game, this.entities[i]);
 			}
-		}
-	};
+		};
+	}
 	
-	scope.EntityManager = new _EntityManager();
+	scope.EntityManager = new EntityManager();
 }(Game));
 
