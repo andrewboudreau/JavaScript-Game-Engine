cf660336acd1b78f757d37f3d0cdd5753296aaaa
Andrew <andrewboudreau@gmail.com>
2012-07-12 01:56:30 -0500

    jshint whit for entitymanager

--- EntityManager.js
+++ EntityManager.js
@@ -1,4 +1,4 @@
-/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, curly:true, browser:true, indent:4, maxerr:50, newcap:true, white:false */
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, curly:true, browser:true, indent:4, maxerr:50, newcap:true, white:true */
 /*global Game Entity */
 (function (scope) {
 	"use strict";
@@ -8,53 +8,53 @@
 	}
 	
 	function EntityManager() {
-		if ( EntityManager.prototype.singletonInstance ) {
+		if (EntityManager.prototype.singletonInstance) {
 			return EntityManager.prototype.singletonInstance;
 		}
 		
 		EntityManager.prototype.singletonInstance = this;
 		this.entities = [];
 		
-		this.guard = function(entity) {
-			if ( !(entity instanceof Entity) ) {
+		this.guard = function (entity) {
+			if (!(entity instanceof Entity)) {
 				throw "entity must be an instance of Entity";
 			}	
 		};
 		
-		this.add = function( entity ) {	
+		this.add = function (entity) {	
 			this.guard(entity);
 			
 			this.entities.push(entity);
 			return entity;
 		};
 		
-		this.remove = function( entity ) {
+		this.remove = function (entity) {
 			var index = this.entities.indexOf(entity);
-			if( index !== -1) {
+			if (index !== -1) {
 				this.entities.splice(index, 1);
 			}
 		};
 		
-		this.clear = function() {
+		this.clear = function () {
 			this.entities.length = 0;
 		};
 		
-		this.indexOf = function( entity ) {
+		this.indexOf = function (entity) {
 			this.guard(entity);
 			
-			return this.entities.indexOf( entity );
+			return this.entities.indexOf(entity);
 		};
 		
-		this.item = function(index) {
-			if(index < 0 || index >= this.entities.length) {
+		this.item = function (index) {
+			if (index < 0 || index >= this.entities.length) {
 				throw "argument out of range";
 			}
 			
 			return this.entities[index];
 		};
 	
-		this.each = function(lambda) {
-			for(var i = 0; i < this.entities.length; i++) {
+		this.each = function (lambda) {
+			for (var i = 0; i < this.entities.length; i++) {
 				lambda.call(Game, this.entities[i]);
 			}
 		};
